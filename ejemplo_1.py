427799c1fa1f99b5fa245fa20962d4a06ec5496e
JorgeA999 <aliriomri@gmail.com>
2025-11-17 17:40:00 -0500

    Calculadora

--- ejemplo_1.py
+++ ejemplo_1.py
@@ -1,1 +1,12 @@
-print('Hola')
+import streamlit as st
+st.title('calculadora simple con streamlit')
+valor1=st.number_input('Ingrese el primer valor:', min_value=0.0, value =10.0)
+valor3=st.number_input('Ingrese el segundo valor:', min_value=0.0, value =5.0)
+st.write('...') # separador visual
+suma=valor1 + valor3
+if st.button('Calcular Suma'):
+    st.success(f'El resultado de la suma es: **{suma}**')
+
+st.subheader('Valores ingresados')
+st.write(f'Primer valor: **{valor1}**')
+st.write(f'segundo valor: ***{valor3}*')
